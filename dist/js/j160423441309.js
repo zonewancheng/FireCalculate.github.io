272790dfbbee8dffb337d298b6aaca3dd623caa1
JoeyCheng <joeycheng@tvunetworks.com>
2023-09-13 04:44:39 +0000

    's'

--- dist/js/j160423441309.js
+++ dist/js/j160423441309.js
@@ -51,8 +51,8 @@ function floatToTimeString(floatTime) {
 function draw(schedule) {
     //console.log(schedule)
     d3.select("#clock").selectAll("*").remove();
-    const width = 800;
-    const height = 800;
+    const width = 700;
+    const height = 700;
     const radius = Math.min(width, height) / 2;
     const centerX = width / 2;
     const centerY = height / 2;
